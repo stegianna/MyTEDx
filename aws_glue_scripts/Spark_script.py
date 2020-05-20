abf2e249cf6af28843effbff6536e9af161cfb21
stegianna <stefanogiannattasio94@gmail.com>
2020-05-20 15:49:37 +0200

    added code to aggregate next talk

--- aws_glue_scripts/Spark_script.py
+++ aws_glue_scripts/Spark_script.py
@@ -2,7 +2,7 @@ import sys
 import json
 import pyspark
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, collect_list, array_join
+from pyspark.sql.functions import col, collect_list, array_join, struct
 
 tedx_dataset_path = "tedx_dataset.csv"
 
@@ -39,3 +39,30 @@ tedx_dataset_agg = tedx_dataset.join(tags_dataset_agg, tedx_dataset.idx == tags_
     .drop("idx") \
 
 tedx_dataset_agg.printSchema()
+
+
+# ADD INFORMATION ABOUT NEXT_TALK 
+next_dataset_path = "watch_next_dataset.csv"
+next_dataset = spark.read.option("header","true").csv(next_dataset_path).distinct().drop('url')
+
+print(f"Number of items from RAW DATA {next_dataset.count()}")
+
+print("Next_dataset schema")
+next_dataset.printSchema()
+
+next_dataset_agg = next_dataset.join(tedx_dataset_agg, tedx_dataset_agg._id == next_dataset.watch_next_idx) \
+    .drop("watch_next_idx").groupBy('idx').agg(collect_list(struct(col('_id').alias('next_id'),'main_speaker','title','url')).alias("next_talks")) \
+        .select('idx','next_talks') 
+
+print("next_dataset_agg schema")
+next_dataset_agg.printSchema()
+
+
+final_dataset = tedx_dataset_agg.join(next_dataset_agg, tedx_dataset_agg._id == next_dataset_agg.idx, 'left') \
+    .drop("idx").select("*")
+
+print("final dataset schema")
+final_dataset.printSchema()
+
+print(final_dataset.toJSON().first())
+
